b52339003081ef79a7f96050dae473d492a62a4b
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-03-04 11:14:43 -0600

    🖊 Add i18n es and ca

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,1 +1,17 @@
-export const getI18n = ()=>{}
+import catalan from './ca.json';
+import spanish from './es.json';
+
+const LANGUAGES = {
+    CATALAN:'ca',
+    SPANISH:'es'
+}
+
+interface Props {
+  currentLocale?: string;
+}
+
+export const getI18N = ({ currentLocale='es' }: Props) => {
+  if (currentLocale === LANGUAGES.CATALAN) return catalan;
+  if (currentLocale === LANGUAGES.SPANISH) return spanish;
+  return spanish;
+};
